c200a310c1cd1abf697a9115c685a417ea4a99b7
seyun0714 <seun0714@naver.com>
2025-03-03 14:13:20 +0900

    vercel analytics 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "./styles/Layout.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Layout() {
   return (
     <div className="layout-wrapper">
       <Outlet />
+      <Analytics />
     </div>
   );
 }
