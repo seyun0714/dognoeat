d5aa2479c9a6e796c04011c9db46ae65e57ab745
seyun0714 <seun0714@naver.com>
2025-03-05 13:51:55 +0900

    SpeedInsights 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import "./styles/Layout.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function Layout() {
   return (
     <div className="layout-wrapper">
       <Outlet />
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
